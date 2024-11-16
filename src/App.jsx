0ae1b90f65da47c84fcadea9b1cc86ecae51b6b3
nasywan22 <munarok22@gmail.com>
2024-11-16 17:09:05 +0700

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import NavigationBar from './navbar.jsx';
 import Content from './content.jsx';
+import Footer from './footer.jsx';
 
 function App() {
   return (
     <>
       <NavigationBar />
       <Content />
+      <Footer />
     </>
   )
 }
---
+++ src/footer.jsx
@@ -0,0 +1,20 @@
+import './footer.css';
+import React, { Component } from "react";
+
+class Footer extends Component {
+    Footer() {
+        return (
+            <div className="footer container-xl h-48 bg-gray-600 flex justify-center items-center">
+                <p className='text-white'>Copyright © 2020 Your Company. All rights reserved.</p>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <this.Footer />
+        );
+    }
+}
+
+export default Footer;
